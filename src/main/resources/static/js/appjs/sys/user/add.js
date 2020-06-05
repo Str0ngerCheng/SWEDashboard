c99a36003e38aff8dc0de49c560092098fe781b3
Rongfeihui <1069396219@qq.com>
2020-06-05 22:27:52 +0800

    R

--- src/main/resources/static/js/appjs/sys/user/add.js
+++ src/main/resources/static/js/appjs/sys/user/add.js
@@ -7,19 +7,21 @@ $.validator.setDefaults({
 		save();
 	}
 });
-//function getCheckedRoles() {
-//	var adIds = "";
-//	$("input:checkbox[name=role]:checked").each(function(i) {
-//		if (0 == i) {
-//			adIds = $(this).val();
-//		} else {
-//			adIds += ("," + $(this).val());
-//		}
-//	});
-//	return adIds;
-//}
+function getCheckedRoles() {
+	var adIds = "";
+	$("input:checkbox[name=role]:checked").each(function(i) {
+		if (0 == i) {
+			adIds = $(this).val();
+	} else {
+			adIds += ("," + $(this).val());
+		}
+	});
+return adIds;
+}
+
+
 function save() {
-//	$("#roleIds").val(getCheckedRoles());
+	$("#roleIds").val(getCheckedRoles());
 	$.ajax({
 		cache : true,
 		type : "POST",
@@ -49,7 +51,17 @@ function validateRule() {
 	$("#signupForm").validate({
 		rules : {
 			name : {
-				required : true
+				required : true,
+				remote : {
+					url :document.getElementsByTagName('meta')['ctx'].content + "/sys/user/exit", // 后台处理程序
+					type : "post", // 数据发送方式
+					dataType : "json", // 接受数据格式
+					data : { // 要传递的数据
+						name : function() {
+							return $("#name").val();
+						}
+					}
+				}
 			},
 			username : {
 				required : true,
@@ -85,9 +97,9 @@ function validateRule() {
 			agree : "required"
 		},
 		messages : {
-
 			name : {
-				required : icon + "请输入姓名"
+				required : icon + "请输入姓名",
+				remote: icon+"用户名已经存在"
 			},
 			username : {
 				required : icon + "请输入您的用户名",
